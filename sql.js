85d34eed89679b571e4209a05ccfb947739b3328
ThinCan <01169581@pw.edu.pl>
2023-10-22 13:59:53 +0200

    html website

--- sql.js
+++ sql.js
@@ -4,7 +4,7 @@ async function workOnDataBase(sql) {
     var connection = await mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "K2I82002@MW",
+        password: "root",
     });
 
     try {
@@ -15,16 +15,16 @@ async function workOnDataBase(sql) {
     }
 }
 
-async function action(sql, data) {
+async function action(sql, data, query_values) {
     var connection = await mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "K2I82002@MW",
+        password: "root",
         database: data
     });
 
     try {
-        var result = await connection.query(sql);
+        var result = await connection.query(sql, query_values);
         return result;
     } finally {
         await connection.end();
@@ -46,10 +46,66 @@ async function test() {
     }
 }
 
+// Dodane na potrzeby index.js
+class DBUser {
+    constructor(name, surname) { 
+        // if(name == null || surname == null) { throw new Error("Name and surname cannot be nulls."); }
+        if(typeof name !== 'string' || typeof surname !== 'string') { throw new Error("Name and surname must be of string types."); }
+        if(name.length === 0 || surname.length === 0) { throw new Error("Name and surname must be at least 1 character long."); }
+        this.name = name;
+        this.surname = surname;
+    }
+}
+
+const INDEX_DB_NAME = "indexdb";
+
+async function index_create_db() {
+    try {
+        await workOnDataBase(`CREATE DATABASE IF NOT EXISTS ${INDEX_DB_NAME}`);
+        await action("CREATE TABLE IF NOT EXISTS users (name VARCHAR(255), surname VARCHAR(255))", INDEX_DB_NAME);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function index_get_user(user) {
+    if(!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
+    
+    const [rows] = await action(
+        'SELECT * FROM `users` WHERE `name` = ? AND `surname` = ?', 
+        INDEX_DB_NAME,
+        [user.name, user.surname]);
+
+    return rows;
+}
+
+async function index_get_all_users() {
+   const [rows] = await action(
+        'SELECT * FROM `users`', INDEX_DB_NAME
+   ) 
+   return rows
+}
+
+async function index_create(user) {
+    if(!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
+    
+    const [result] = await action(
+        'INSERT INTO `users` (`name`, `surname`) VALUES (?, ?)', 
+        INDEX_DB_NAME,
+        [user.name, user.surname]);
+
+    return result;
+}
+
+// Dodane na potrzeby index.js
+
 //test()
 
 module.exports = {
     workOnDataBase,
-    action
+    action,
+
+    //Dodane na potrzeby index.js
+    index_create_db, index_get_user, index_get_all_users, index_create, DBUser
 };
 
