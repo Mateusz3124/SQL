158ca145fbf1c22e04781f118d067376200b7ec5
Krzysztof Jastrzębski <krzysztof@jastrzebski>
2023-11-04 12:43:08 +0100

    id i nowe testy

--- sql.js
+++ sql.js
@@ -35,10 +35,10 @@ async function action(sql, db, query_values) {
 
 // Dodane na potrzeby index.js
 class DBUser {
-    constructor(name, surname) { 
+    constructor(name, surname) {
         // if(name == null || surname == null) { throw new Error("Name and surname cannot be nulls."); }
-        if(typeof name !== 'string' || typeof surname !== 'string') { throw new Error("Name and surname must be of string types."); }
-        if(name.length === 0 || surname.length === 0) { throw new Error("Name and surname must be at least 1 character long."); }
+        if (typeof name !== 'string' || typeof surname !== 'string') { throw new Error("Name and surname must be of string types."); }
+        if (name.length === 0 || surname.length === 0) { throw new Error("Name and surname must be at least 1 character long."); }
         this.name = name;
         this.surname = surname;
     }
@@ -47,62 +47,76 @@ class DBUser {
 async function index_create_db(name) {
     try {
         await workOnDataBase(`CREATE DATABASE IF NOT EXISTS ${name}`);
-        await action("CREATE TABLE IF NOT EXISTS users (name VARCHAR(255), surname VARCHAR(255))", name);
+        await action("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255))", name);
     } catch (err) {
         console.error(err);
     }
 }
 
 async function index_get_user(user, DBname) {
-    if(!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
-    
+    if (!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
+
     const [rows] = await action(
-        'SELECT * FROM `users` WHERE `name` = ? AND `surname` = ?', 
+        'SELECT * FROM `users` WHERE `name` = ? AND `surname` = ?',
         DBname,
         [user.name, user.surname]);
 
     return rows;
 }
 
+async function index_get_user_id(user, DBname) {
+    if (!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
+
+    const [rows] = await action(
+        'SELECT id FROM `users` WHERE `name` = ? AND `surname` = ?',
+        DBname,
+        [user.name, user.surname]);
+    console.log(rows[0].id)
+    return rows[0].id;
+}
+
 async function index_get_all_users(name) {
-   const [rows] = await action(
+    const [rows] = await action(
         'SELECT * FROM `users`', name
-   ) 
-   return rows
+    )
+    return rows
 }
 
 async function index_create_user(user, DBname) {
-    if(!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
-    
+    if (!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
+
     const [result] = await action(
-        'INSERT INTO `users` (`name`, `surname`) VALUES (?, ?)', 
+        'INSERT INTO `users` (`name`, `surname`) VALUES (?, ?)',
         DBname,
         [user.name, user.surname]);
 
     return result;
 }
 
-async function index_delete_user(user, DBname) {
-    if (!(user instanceof DBUser)) { throw new Error("User parameter must be of type DBUser."); }
-    
+async function index_delete_user(id, DBname) {
     const [result] = await action(
-        'DELETE FROM `users` WHERE `name` = ? AND `surname` = ?', 
+        'DELETE FROM `users` WHERE `id` = ? ',
         DBname,
-        [user.name, user.surname]
+        [id]
     );
-
+    if (result.affectedRows === 0) {
+        throw new Error("User with the given ID not found");
+    }
     return result;
 }
 
-async function index_update_user(user_toupdate, user_updated, DBname) {
+async function index_update_user(user_id, user_updated, DBname) {
     const [result] = await action(
-        'UPDATE `users` SET `name` = ?, `surname` = ? WHERE `name` = ? AND `surname` = ?', 
+        'UPDATE `users` SET `name` = ?, `surname` = ? WHERE `id` = ?',
         DBname,
-        [user_updated.name, user_updated.surname, user_toupdate.name, user_toupdate.surname]
+        [user_updated.name, user_updated.surname, user_id]
     );
-
+    if (result.affectedRows === 0) {
+        throw new Error("User with the given ID not found");
+    }
     return result;
 }
+
 
 
 // Dodane na potrzeby index.js
@@ -112,6 +126,7 @@ module.exports = {
     action,
     index_create_db,
     index_get_user,
+    index_get_user_id,
     index_get_all_users,
     index_create_user,
     index_delete_user,
--- sql.test.js
+++ sql.test.js
@@ -6,7 +6,7 @@ describe("MySQL Functions", () => {
 
     afterAll(async () => {
         await sqljs.workOnDataBase("DROP DATABASE IF EXISTS mytest");
-    });
+    });   
 
     test("should create a database with table", async () => {
         await sqljs.index_create_db(INDEX_DB_NAME);
@@ -23,23 +23,68 @@ describe("MySQL Functions", () => {
         expect(result.some((obj) => obj.name === 'Marcin' && obj.surname === 'Kwiatkowski')).toBe(true);
     });
 
+    test.each([
+        ["", "Kwiatkowski"],
+        ["Jan", ""],
+        ["", ""],
+    ])("index_create should throw an exception when new user name or surname is empty", async (name, surname) => {
+        try {
+            const newuser = new sqljs.DBUser(name, surname);
+        } catch (err) {
+            expect(err.message).toBe("Name and surname must be at least 1 character long.");
+        }
+    });  
+    
+    
+    test.each([
+        [1, "Kwiatkowski"],
+        ["Jan", 1],
+        [1, 1],
+    ])("index_create should throw an exception when new user name or surname is not string", async (name, surname) => {
+        try {
+            const newuser = new sqljs.DBUser(name, surname);
+        } catch (err) {
+            expect(err.message).toBe("Name and surname must be of string types.");
+        }
+    }); 
+
     test("index_update should update given information", async () => {
         const olduser = new sqljs.DBUser("Marcin", "Kwiatkowski")
         const newuser = new sqljs.DBUser("Marcin", "Bogaczewicz")
-        await sqljs.index_update_user(olduser, newuser, INDEX_DB_NAME)
+        const id = await sqljs.index_get_user_id(olduser, INDEX_DB_NAME)
+        await sqljs.index_update_user(id, newuser, INDEX_DB_NAME)
         const [result] = await sqljs.action("SELECT * FROM users", INDEX_DB_NAME);
         expect(result.some((obj) => obj.name === 'Marcin' && obj.surname === 'Bogaczewicz')).toBe(true);
     });
 
+
+    test("index_update should throw exception when user with given id doesnt exist", async () => {
+        try {
+            const newuser = new sqljs.DBUser("Marcin", "Bogaczewicz")
+            await sqljs.index_update_user(10, newuser, INDEX_DB_NAME)
+        } catch (err) {
+            expect(err.message).toBe("User with the given ID not found");
+        }
+    });
+
     test("index_delete should delete information", async () => {
-        await sqljs.action("INSERT INTO users (name, surname) VALUES ('Marcin', 'Socha')",INDEX_DB_NAME);
+        await sqljs.action("INSERT INTO users (name, surname) VALUES ('Marcin', 'Socha')", INDEX_DB_NAME);
         const [result] = await sqljs.action("SELECT * FROM users", INDEX_DB_NAME);
         const test1 = result.some((obj) => obj.name === 'Marcin' && obj.surname === 'Socha')
-        const userToDelete = new sqljs.DBUser("Marcin", "Socha")
-        await sqljs.index_delete_user(userToDelete, INDEX_DB_NAME);
+        const usertodelete = new sqljs.DBUser("Marcin", "Socha")
+        const id = await sqljs.index_get_user_id(usertodelete, INDEX_DB_NAME)
+        await sqljs.index_delete_user(id, INDEX_DB_NAME);
         const [result2] = await sqljs.action("SELECT * FROM users", INDEX_DB_NAME);
         const test2 = result2.some((obj) => obj.name === 'Marcin' && obj.surname === 'Socha')
         expect(test1 && !test2).toBe(true);
+    });
+    
+    test("index_delete should throw exception when user with given id doesnt exist", async () => {
+        try {
+            await sqljs.index_delete_user(10, INDEX_DB_NAME)
+        } catch (err) {
+            expect(err.message).toBe("User with the given ID not found");
+        }
     });
 
 });
